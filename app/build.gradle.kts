731ec6b2b43105a494e097c35ce92a3a9404cdf8
肥宅水水呀 <91878264+happycola233@users.noreply.github.com>
2026-02-09 01:37:22 +0800

    chore(release): 更新版本号至 v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.happycola233.bilitools"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
